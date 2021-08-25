ad03c73504e48e6c1d8e6ba63541bb5bc8631bf6
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-25 08:49:58 +0800

    2.4 props, parent, child comp

--- 02-no-frills-react/src/App.js
+++ 02-no-frills-react/src/App.js
@@ -1,18 +1,25 @@
 const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Luna"),
-    React.createElement("h3", {}, "Dog"),
-    React.createElement("h3", {}, "Havanese"),
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
-    ...[1, 2, 3, 4].map((i) => React.createElement("h2", {}, i)),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
   ]);
 };
 
