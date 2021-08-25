1c5a0699d6df9f63a1dc70099ad44ed9723b0850
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-25 08:39:20 +0800

    2.3 nested components

---
+++ 02-no-frills-react/src/App.js
@@ -0,0 +1,19 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Luna"),
+    React.createElement("h3", {}, "Dog"),
+    React.createElement("h3", {}, "Havanese"),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    ...[1, 2, 3, 4].map((i) => React.createElement("h2", {}, i)),
+    React.createElement(Pet),
+    React.createElement(Pet),
+    React.createElement(Pet),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
