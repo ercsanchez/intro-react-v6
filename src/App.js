a34493a1145ff44cf06d51d01398d25c08efc019
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2021-08-28 15:33:10 +0800

    6.3 create context and provide

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import ReactDOM from "react-dom";
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeState = useState("darkblue");
+
   return (
-    <div>
-      <Router>
-        <header>
-          <Link to="/">
-            <h1>Adopt Me!</h1>
-          </Link>
-        </header>
-        <Switch>
-          <Route path="/details/:id">
-            <Details />
-          </Route>
-          <Route path="/">
-            <SearchParams />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <ThemeContext.Provider value={themeState}>
+      <div>
+        <Router>
+          <header>
+            <Link to="/">
+              <h1>Adopt Me!</h1>
+            </Link>
+          </header>
+          <Switch>
+            <Route path="/details/:id">
+              <Details />
+            </Route>
+            <Route path="/">
+              <SearchParams />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
